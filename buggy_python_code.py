49a9e8fde44191827a543633221e554338c80889
unknown <xuhlik2@ad.fi.muni.cz>
2022-04-27 16:35:30 +0200

    correcting bugs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,9 +1,8 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
 import subprocess
 import base64
-import subprocess
+import cPickle
 import flask
 
 # Input injection
@@ -13,13 +12,13 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def checker(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
-class RunBinSh(object):
+class RunBinSh:
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
